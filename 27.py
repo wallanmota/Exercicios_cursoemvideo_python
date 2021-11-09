3a658a2be98b15825ab0dbf4581fb29578903210
wallanmota <wallanmota.13@gmail.com>
2021-11-09 16:55:34 -0300

    ajuste27

--- 27.py
+++ 27.py
@@ -10,42 +10,7 @@ nome_dividido = nome.split()
 print('O primeiro nome da pesso é {}'.format(nome_dividido[0]))
 print('O ultimo nome da pesso é {}'.format(nome_dividido[len(nome_dividido)-1]))
 
-tecnologia
-pessoas
-processos
 
-carreiras
-proximo passsos *
-quantas filiais - full time remoto
-nivel
-fomacao
-inicio imediato
-horario
 
-'''
-Case:
-visualizaçao das perguntas por grupos
-separar em funcoes
-teste unitario = bem legal
-clean code
-resposta com grafico = bem vindo
-todas as perguntas do doc sao obrigatorias / posso colocar mais
 
-focar no caso de uso geral
 
-A apresentacao tem um tempo
-repo: AfroDev-WallanMota
-
-Segue meu contato também  11 99607-3271, meu email mgalmeida@alelo.com.br
-'''
-'''
-salario: 3.500 jr
-vr/va: 1600 sem desconto
-assitencia bradesco - Apartamento
-previdencia privada
-ppr 2,56 salarios
-200 multibeneficio farmacia
-750 - alelo natal
-alelitos = moeda propria, reverter na plataforma online
-70% bolsa faculdade
-'''
