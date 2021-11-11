0ec4227fe1f0e12e53f6af21fd914320a0e9a730
wallanmota <wallanmota.13@gmail.com>
2021-11-11 15:48:00 -0300

    ex34

---
+++ 34.py
@@ -0,0 +1,14 @@
+'''
+Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+Para salários superiores a R$1250,00, calcule um aumento de 10%.
+Para os inferiores ou iguais, o aumento é de 15
+'''
+
+salario = float(input('Qual o salário do funcionário? R$'))
+
+if salario > 1250:
+    aumento = salario * 10 / 100 + salario
+else:
+    aumento = salario * 15 / 100 + salario
+
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora!'.format(salario, aumento))
